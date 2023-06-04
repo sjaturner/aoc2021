ae804acd5696f2ce5cd31766d2d21c9424534705
sjaturner <sjaturner@googlemail.com>
2023-06-04 14:31:49 +0100

    first attempt, check it though

--- day1/increases/src/main.rs
+++ day1/increases/src/main.rs
@@ -2,8 +2,21 @@ use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
+    let mut last: Option<i32> = None;
+    let mut increased = 0;
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from standard in");
-        println!("{}", line);
+        let depth: i32 = line.parse().unwrap();
+
+        match last {
+            Some(val) => {
+                if depth > val {
+                    increased += 1;
+                }
+            }
+            _ => (),
+        }
+        last = Some(depth);
     }
+    println!("{increased}")
 }
