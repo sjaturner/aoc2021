ac2abd9e8e8cd3f94f1dba0880e67519321ded0f
sjaturner <sjaturner@googlemail.com>
2023-06-04 14:14:48 +0100

    add the files

---
+++ day1/increases/src/main.rs
@@ -0,0 +1,9 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+        println!("{}", line);
+    }
+}
