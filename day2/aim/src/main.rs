456e7905299cfc3fe145514926975825b8d551f8
sjaturner <sjaturner@googlemail.com>
2023-06-05 20:37:20 +0100

    this worked too

--- day2/aim/src/main.rs
+++ day2/aim/src/main.rs
@@ -4,15 +4,19 @@ fn main() {
     let stdin = io::stdin();
     let mut depth = 0;
     let mut forward = 0;
+    let mut aim = 0;
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from standard in");
         let line: Vec<&str> = line.split(' ').collect();
         let val = line[1].parse::<i32>().unwrap();
 
         match line[0] {
-            "up" => depth -= val,
-            "down" => depth += val,
-            "forward" => forward += val,
+            "up" => aim -= val,
+            "down" => aim += val,
+            "forward" => {
+                forward += val;
+                depth += aim * val;
+            }
             &_ => todo!(),
         }
     }
