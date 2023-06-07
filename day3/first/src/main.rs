660049cb434627a650913f43dd53bac4a424d5e1
sjaturner <sjaturner@googlemail.com>
2023-06-07 19:00:33 +0100

    well that worked

--- day3/first/src/main.rs
+++ day3/first/src/main.rs
@@ -2,16 +2,41 @@ use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
-    let mut bits: Option<usize> = None;
+    let mut ones = Vec::new();
+    let mut zero = Vec::new();
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from standard in");
         let line = line.trim();
         let length = line.len();
 
-        if bits.is_none() {
-            bits = Some(length);
-        } else {
-            assert!(bits == Some(length));
+        if ones.len() == 0 {
+            for _ in 0..length {
+                ones.push(0);
+                zero.push(0);
+            }
+        } else if ones.len() != length {
+            assert!(false);
+        }
+
+        for (index, character) in line.chars().rev().enumerate() {
+            if character == '1' {
+                ones[index] += 1;
+            } else {
+                zero[index] += 1;
+            }
         }
     }
+    let mut gamma = 0;
+    let mut epsilon = 0;
+    for index in 0..ones.len() {
+        if ones[index] > zero[index] {
+            gamma |= 1 << index;
+        } else if ones[index] < zero[index] {
+            epsilon |= 1 << index;
+        } else {
+            assert!(false);
+        }
+    }
+
+    println!("{gamma} {epsilon} {}", gamma * epsilon);
 }
