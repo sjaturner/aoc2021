c99198a5fa6d28052492bc6cd14e570efce93cf3
sjaturner <sjaturner@googlemail.com>
2023-06-22 19:34:05 +0100

    for small numbers of days

--- day6/first/src/main.rs
+++ day6/first/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io;
 
 fn main() {
@@ -16,4 +17,23 @@ fn main() {
         .map(|val| val.parse::<u32>().unwrap())
         .collect::<Vec<u32>>();
     println!("{:?}", fish_state);
+
+    let args: Vec<String> = env::args().collect();
+    println!("{}", args[1].parse::<u32>().unwrap());
+
+    for day in 1..=args[1].parse::<u32>().unwrap() {
+        let len = fish_state.len();
+        for index in 0..len {
+            if fish_state[index] == 0 {
+                fish_state.push(8);
+                fish_state[index] = 6;
+            } else {
+                fish_state[index] -= 1;
+            }
+        }
+        if false {
+            println!("{day} {:?}", fish_state);
+        }
+    }
+    println!("{}", fish_state.len());
 }
