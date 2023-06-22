aff2f049f933c23c3d31864e35568159a10ad563
sjaturner <sjaturner@googlemail.com>
2023-06-22 19:09:31 +0100

    add the files

---
+++ day6/first/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    let reader = io::stdin();
+    let mut buffer: String = String::new();
+
+    reader
+        .read_line(&mut buffer)
+        .ok()
+        .expect("error")
+        .to_string();
+
+    let mut fish_state = buffer
+        .trim()
+        .split(',')
+        .map(|val| val.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+    println!("{:?}", fish_state);
+}
