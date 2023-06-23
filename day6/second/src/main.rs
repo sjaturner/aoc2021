460ed75694d8c42dfaad55feae79c23a03e34778
sjaturner <sjaturner@googlemail.com>
2023-06-23 07:57:39 +0100

    that worked, about to get tougher

--- day6/second/src/main.rs
+++ day6/second/src/main.rs
@@ -11,29 +11,43 @@ fn main() {
         .expect("error")
         .to_string();
 
-    let mut fish_state = buffer
+    let fish_state = buffer
         .trim()
         .split(',')
         .map(|val| val.parse::<u32>().unwrap())
         .collect::<Vec<u32>>();
-    println!("{:?}", fish_state);
+
+    if false {
+        println!("{:?}", fish_state);
+    }
 
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[1].parse::<u32>().unwrap());
 
-    for day in 1..=args[1].parse::<u32>().unwrap() {
-        let len = fish_state.len();
-        for index in 0..len {
-            if fish_state[index] == 0 {
-                fish_state.push(8);
-                fish_state[index] = 6;
-            } else {
-                fish_state[index] -= 1;
-            }
-        }
-        if false {
-            println!("{day} {:?}", fish_state);
-        }
+    if false {
+        println!("{}", args[1].parse::<u32>().unwrap());
     }
-    println!("{}", fish_state.len());
+
+    let mut fish_buckets: [u64; 9] = [0; 9];
+
+    for state in &fish_state {
+        fish_buckets[*state as usize] += 1;
+    }
+
+    for _ in 1..=args[1].parse::<u32>().unwrap() {
+        let mut next_buckets: [u64; 9] = [0; 9];
+
+        if false {
+            println!("{:?}", fish_buckets);
+        }
+
+        let store = fish_buckets[0];
+        for bucket in 0..(fish_buckets.len() - 1) {
+            next_buckets[bucket] = fish_buckets[bucket + 1];
+        }
+        next_buckets[6] += store;
+        next_buckets[8] = store;
+        fish_buckets.copy_from_slice(&next_buckets)
+    }
+
+    println!("{}", fish_buckets.into_iter().sum::<u64>());
 }
