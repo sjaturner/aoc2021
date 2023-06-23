84f3fa67c0b71f9268ea6fd1120f41687a84c253
sjaturner <sjaturner@googlemail.com>
2023-06-23 18:57:20 +0100

    the data

--- day7/first/src/main.rs
+++ day7/first/src/main.rs
@@ -17,23 +17,4 @@ fn main() {
         .map(|val| val.parse::<u32>().unwrap())
         .collect::<Vec<u32>>();
     println!("{:?}", fish_state);
-
-    let args: Vec<String> = env::args().collect();
-    println!("{}", args[1].parse::<u32>().unwrap());
-
-    for day in 1..=args[1].parse::<u32>().unwrap() {
-        let len = fish_state.len();
-        for index in 0..len {
-            if fish_state[index] == 0 {
-                fish_state.push(8);
-                fish_state[index] = 6;
-            } else {
-                fish_state[index] -= 1;
-            }
-        }
-        if false {
-            println!("{day} {:?}", fish_state);
-        }
-    }
-    println!("{}", fish_state.len());
 }
