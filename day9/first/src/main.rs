b5c54e04a289ae6f5b06f4677dd9d9d716bf22d7
sjaturner <sjaturner@googlemail.com>
2023-06-29 12:32:39 +0100

    good enough

--- day9/first/src/main.rs
+++ day9/first/src/main.rs
@@ -1,7 +1,37 @@
 use std::io::{self, BufRead};
 
-fn get_cell(row: i32, col: i32) -> Option<i32> {
-    None
+fn get_cell(array: &Vec<Vec<i32>>, row: i32, col: i32) -> Option<i32> {
+    let rows = array.len() as i32;
+    let cols = array[0].len() as i32;
+
+    if row >= rows || row < 0 || col >= cols || col < 0 {
+        None
+    } else {
+        Some(array[row as usize][col as usize])
+    }
+}
+
+fn is_low_point(array: &Vec<Vec<i32>>, row: i32, col: i32) -> Option<i32> {
+    let current_cell = get_cell(array, row, col).unwrap();
+    let deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mut matches = 0;
+    let mut possible = 0;
+
+    for delta in deltas {
+        if let Some(value) = get_cell(array, row + delta.0, col + delta.1) {
+            possible += 1;
+            if value <= current_cell {
+                return None;
+            } else {
+                matches += 1;
+            }
+        }
+    }
+    if matches == possible {
+        Some(current_cell)
+    } else {
+        None
+    }
 }
 
 fn main() {
@@ -16,7 +46,16 @@ fn main() {
                 .collect(),
         );
     }
+    let rows = array.len() as i32;
+    let cols = array[0].len() as i32;
+    let mut risk_level_accumulation = 0;
 
-    let rows = array.len();
-    let cols = array[0].len();
+    for col in 0..cols {
+        for row in 0..rows {
+            if let Some(value) = is_low_point(&array, row, col) {
+                risk_level_accumulation += value + 1;
+            }
+        }
+    }
+    println!("{risk_level_accumulation}");
 }
