ee2c4dfeea0ea988a8b42207ef95c6806cf281e4
sjaturner <sjaturner@googlemail.com>
2023-06-29 12:01:45 +0100

    uh oh might struct

--- day9/first/src/main.rs
+++ day9/first/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{self, BufRead};
+
+fn get_cell(row: i32, col: i32) -> Option<i32> {
+    None
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut array: Vec<Vec<i32>> = Vec::new();
+
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+        array.push(
+            line.chars()
+                .map(|digit| (digit as i32 - '0' as i32))
+                .collect(),
+        );
+    }
+
+    let rows = array.len();
+    let cols = array[0].len();
 }
