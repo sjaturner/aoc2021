71e38439e830278c39f9325932937eff5261ae32
sjaturner <sjaturner@googlemail.com>
2023-06-30 20:39:47 +0100

    yet again, ugly but adequate

--- day9/second/src/main.rs
+++ day9/second/src/main.rs
@@ -11,7 +11,7 @@ fn get_cell(array: &Vec<Vec<i32>>, row: i32, col: i32) -> Option<i32> {
     }
 }
 
-fn basin(array: &Vec<Vec<i32>>, row: i32, col: i32) {
+fn basin(array: &Vec<Vec<i32>>, row: i32, col: i32) -> u32 {
     let deltas = [(-1, 0), (0, 1), (1, 0), (0, -1)];
     let mut basin_cells: Vec<(i32, i32)> = Vec::new();
     basin_cells.push((row, col));
@@ -19,26 +19,39 @@ fn basin(array: &Vec<Vec<i32>>, row: i32, col: i32) {
 
     loop {
         let basin_cells_len = basin_cells.len();
+        let mut added = false;
         for index in top..basin_cells_len {
             let (row_test, col_test) = basin_cells[index];
             if let Some(center) = get_cell(array, row_test, col_test) {
+                if false {
+                    println!("center {:?}", center);
+                }
                 for delta in deltas {
-                    let row = row + delta.0;
-                    let col = col + delta.1;
+                    let row = row_test + delta.0;
+                    let col = col_test + delta.1;
                     if let Some(peripheral) = get_cell(array, row, col) {
-                        if peripheral >= center {
+                        if false {
+                            println!("    peripheral {:?} {row} {col}", peripheral);
+                        }
+                        if peripheral != 9 && peripheral > center {
                             basin_cells.push((row, col));
+                            added = true;
                         }
                     }
                 }
             }
         }
-
-        if basin_cells_len == basin_cells.len() {
+        if false {
+            println!();
+        }
+        if !added {
             break;
         }
-        top = basin_cells.len()
+        top = basin_cells_len;
     }
+    basin_cells.sort();
+    basin_cells.dedup();
+    basin_cells.len() as u32
 }
 
 fn is_low_point(array: &Vec<Vec<i32>>, row: i32, col: i32) -> Option<i32> {
@@ -78,14 +91,18 @@ fn main() {
     }
     let rows = array.len() as i32;
     let cols = array[0].len() as i32;
-    let mut risk_level_accumulation = 0;
+    let mut results: Vec<u32> = Vec::new();
 
     for col in 0..cols {
         for row in 0..rows {
-            if let Some(value) = is_low_point(&array, row, col) {
-                risk_level_accumulation += value + 1;
+            if is_low_point(&array, row, col) != None {
+                results.push(basin(&array, row, col));
             }
         }
     }
-    println!("{risk_level_accumulation}");
+    results.sort();
+    results.reverse();
+    results.truncate(3);
+    let product = results.iter().product::<u32>();
+    println!("{:?}", product);
 }
