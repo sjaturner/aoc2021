d36e8069f7c339aacbacf226ab368606af5fd282
sjaturner <sjaturner@googlemail.com>
2023-07-02 09:17:55 +0100

    that is more like it

--- day10/first/src/main.rs
+++ day10/first/src/main.rs
@@ -1,3 +1,54 @@
+use std::io::{self, BufRead};
+
+fn closer(open: char) -> char {
+    return match open {
+        '[' => ']',
+        '(' => ')',
+        '{' => '}',
+        '<' => '>',
+        _ => panic!("cannot close {}", open),
+    };
+}
+
+fn opener(open: char) -> char {
+    return match open {
+        ']' => '[',
+        ')' => '(',
+        '}' => '{',
+        '>' => '<',
+        _ => panic!("cannot close {}", open),
+    };
+}
+
+fn illegal_score(open: char) -> u32 {
+    return match open {
+        ']' => 57,
+        ')' => 3,
+        '}' => 1197,
+        '>' => 23517,
+        _ => panic!("cannot close {}", open),
+    };
+}
+
+fn balance(stack: &mut Vec<char>, close: char) {
+    if let Some(character) = stack.pop() {
+        if character != opener(close) {
+            println!("expected {} saw {}", closer(character), close);
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+        let mut stack = Vec::new();
+        for bracket in line.chars() {
+            match bracket {
+                '[' | '(' | '{' | '<' => stack.push(bracket),
+                ']' | ')' | '}' | '>' => balance(&mut stack, bracket),
+                _ => panic!("did not expect {}", bracket),
+            }
+        }
+    }
 }
