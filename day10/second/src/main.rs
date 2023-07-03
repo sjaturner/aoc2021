09743a51423c6692f928e934b736ba9c9782f7e2
sjaturner <sjaturner@googlemail.com>
2023-07-03 10:00:51 +0100

    again, seemingly good enough

--- day10/second/src/main.rs
+++ day10/second/src/main.rs
@@ -30,6 +30,16 @@ fn illegal_score(open: char) -> u32 {
     };
 }
 
+fn compete_score(bracket: char) -> u64 {
+    return match bracket {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => panic!("cannot close {}", bracket),
+    };
+}
+
 fn balance(stack: &mut Vec<char>, close: char) -> u32 {
     if let Some(character) = stack.pop() {
         if character != opener(close) {
@@ -43,9 +53,10 @@ fn balance(stack: &mut Vec<char>, close: char) -> u32 {
 }
 fn main() {
     let stdin = io::stdin();
-    let mut sum = 0;
+    let mut results = Vec::new();
 
     for line in stdin.lock().lines() {
+        let mut corrupted = false;
         let line = line.expect("Could not read line from standard in");
         let mut stack = Vec::new();
         'line: for bracket in line.chars() {
@@ -54,13 +65,33 @@ fn main() {
                 ']' | ')' | '}' | '>' => {
                     let result = balance(&mut stack, bracket);
                     if result != 0 {
-                        sum += result;
+                        corrupted = true;
                         break 'line;
                     }
                 }
                 _ => panic!("did not expect {}", bracket),
             }
         }
+        if !corrupted && stack.len() != 0 {
+            let atoms = stack
+                .iter()
+                .rev()
+                .map(|bracket| closer(*bracket))
+                .map(|bracket| compete_score(bracket))
+                .collect::<Vec<u64>>();
+            let mut accumulator = 0u64;
+            for atom in atoms {
+                accumulator *= 5;
+                accumulator += atom;
+            }
+            results.push(accumulator);
+        }
     }
-    println!("{sum}");
+    results.sort();
+    assert!(results.len() % 2 == 1);
+    let midpoint = (results.len()) / 2;
+    if false {
+        println!("{:?}", results);
+    }
+    println!("   {:?}", results[midpoint]);
 }
