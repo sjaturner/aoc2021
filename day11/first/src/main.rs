07e30926b600e39b71a63ef8990d3539d9df1268
sjaturner <sjaturner@googlemail.com>
2023-07-05 16:26:43 +0100

    on the plane

--- day11/first/src/main.rs
+++ day11/first/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{self, BufRead};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Cave {
     rows: i32,
     cols: i32,
@@ -42,11 +42,32 @@ impl Cave {
     fn set(&mut self, row: i32, col: i32, val: i32) {
         self.cells[(row * self.cols) as usize + col as usize] = val;
     }
-}
+    fn blank(&mut self, fill: i32) -> Cave {
+        let mut ret = Cave {
+            rows: self.rows,
+            cols: self.cols,
+            cells: Vec::new(),
+        };
+        for _ in 0..self.rows * self.cols {
+            ret.cells.push(fill);
+        }
+        ret
+    }
+    fn add(&mut self, vals: &Cave) -> Cave {
+        let mut ret = self.blank(0);
+        for index in 0usize..(self.rows * self.cols) as usize {
+            let before = self.cells[index];
+            let after = self.cells[index] + vals.cells[index];
 
+            if before < 10 && after >= 10 {
+            }
+        }
+        ret
+    }
+}
 fn main() {
     let cave = Cave::load();
 
     println!("{:?}", cave);
-    println!("{:?}", cave.get(9,9));
+    let mut new = cave.clone();
 }
