17e8bb2efefeb91629a80b6c81ed1cbdf2d33f0e
sjaturner <sjaturner@googlemail.com>
2023-07-05 14:49:33 +0100

    a trivial refactor for load

--- day11/first/src/main.rs
+++ day11/first/src/main.rs
@@ -8,6 +8,34 @@ struct Cave {
 }
 
 impl Cave {
+    fn load() -> Cave {
+        let stdin = io::stdin();
+        let mut cave = Cave {
+            rows: 0,
+            cols: 0,
+            cells: Vec::new(),
+        };
+        let mut rows = 0;
+        for line in stdin.lock().lines() {
+            let line = line.expect("Could not read line from standard in");
+
+            let mut cols = 0;
+            for value in line.chars().map(|digit| digit as i32 - '0' as i32) {
+                cave.cells.push(value);
+                cols += 1;
+            }
+
+            if cave.cols == 0 {
+                cave.cols = cols;
+            } else {
+                assert!(cave.cols == cols);
+            }
+
+            rows += 1;
+        }
+        cave.rows = rows;
+        cave
+    }
     fn get(&self, row: i32, col: i32) -> i32 {
         self.cells[(row * self.cols) as usize + col as usize]
     }
@@ -17,29 +45,8 @@ impl Cave {
 }
 
 fn main() {
-    let stdin = io::stdin();
-    let mut cave = Cave {
-        rows: 0,
-        cols: 0,
-        cells: Vec::new(),
-    };
-    let mut rows = 0;
-    for line in stdin.lock().lines() {
-        let line = line.expect("Could not read line from standard in");
+    let cave = Cave::load();
 
-        let mut cols = 0;
-        for value in line.chars().map(|digit| digit as i32 - '0' as i32) {
-            cave.cells.push(value);
-            cols += 1;
-        }
-
-        if cave.cols == 0 {
-            cave.cols = cols;
-        } else {
-            assert!(cave.cols == cols);
-        }
-
-        rows += 1;
-    }
-    cave.rows = rows;
+    println!("{:?}", cave);
+    println!("{:?}", cave.get(9,9));
 }
