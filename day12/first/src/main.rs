420438a01b5f5b111d7b50c8e5b86ccdca8cef1a
sjaturner <sjaturner@googlemail.com>
2023-07-08 09:57:06 +0100

    sort of canonical network map but integers might be better

--- day12/first/src/main.rs
+++ day12/first/src/main.rs
@@ -1,8 +1,16 @@
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 fn main() {
     let stdin = io::stdin();
+    let mut network: HashMap<String, Vec<String>> = HashMap::new();
+
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from standard in");
+        let nodes: Vec<&str> = line.split("-").collect();
+        let a = nodes[0].to_string();
+        let b = nodes[1].to_string();
+        network.entry(a).or_insert(Vec::new()).push(b);
     }
+    println!("{:?}", network.get("A").unwrap());
 }
