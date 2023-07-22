77ff1d9f3eef614c41c104ef211bd55620b24bbb
sjaturner <sjaturner@googlemail.com>
2023-07-22 15:09:06 +0100

    this is a bit yuck

--- day14/second/src/main.rs
+++ day14/second/src/main.rs
@@ -3,19 +3,38 @@ use std::env;
 use std::io::{self, BufRead};
 use std::process::exit;
 
-#[derive(Debug)]
-struct Pair {
-    chars: (char, char),
-    insert: Option<char>,
+#[derive(Copy, Clone)]
+struct Tally {
+    letters: [u64; 'Z' as usize - 'A' as usize],
 }
 
-fn recurse(lut: &HashMap<(char, char), char>, recur: i32, l: char, r: char) {
+impl Default for Tally {
+    fn default() -> Tally {
+        Tally {
+            letters: [0; 'Z' as usize - 'A' as usize],
+        }
+    }
+}
+
+impl Tally {
+    fn inc(&mut self, c: char) {
+        self.letters[c as usize] += 1;
+    }
+}
+
+fn recurse(
+    lut: &HashMap<(char, char), char>,
+    recur: i32,
+    memo: &mut [[Tally; 'Z' as usize - 'A' as usize]; 'Z' as usize - 'A' as usize],
+    l: char,
+    r: char,
+) {
     if let Some(insertion) = lut.get(&(l, r)) {
         let m = *insertion;
 
         if recur > 0 {
-            recurse(lut, recur - 1, l, m);
-            recurse(lut, recur - 1, m, r);
+            recurse(lut, recur - 1, memo, l, m);
+            recurse(lut, recur - 1, memo, m, r);
         } else {
             print!("{} ", l);
             print!("{} ", m);
@@ -25,10 +44,12 @@ fn recurse(lut: &HashMap<(char, char), char>, recur: i32, l: char, r: char) {
     }
 }
 
+const LETTERS: usize = 'Z' as usize - 'A' as usize;
 fn main() {
     let stdin = io::stdin();
     let mut seed: Vec<char> = Vec::new();
     let mut lut: HashMap<(char, char), char> = HashMap::new();
+    let mut memo: [[Tally; LETTERS]; LETTERS] = [[Tally::default(); LETTERS]; LETTERS];
 
     let args: Vec<_> = env::args().collect();
 
@@ -59,7 +80,7 @@ fn main() {
     }
 
     if depth > 0 {
-        recurse(&lut, depth - 1, 'N', 'N');
+        recurse(&lut, depth - 1, &mut memo, 'N', 'N');
     }
     println!();
 }
