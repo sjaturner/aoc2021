00cf40d5bc183e02f0a0ebceec0e5bcc5a67be59
sjaturner <sjaturner@googlemail.com>
2023-07-29 18:53:14 +0100

    made a start at getting the bits into a vector

--- day16/first/src/main.rs
+++ day16/first/src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{self, BufRead};
+
+fn nibble(character: char) -> u32 {
+    match character {
+        'A'..='F' => character as u32 - 'A' as u32 + 0xa,
+        'a'..='a' => character as u32 - 'a' as u32 + 0xa,
+        '0'..='9' => character as u32 - '0' as u32 + 0x0,
+        _ => todo!(),
+    }
+}
+
+fn line_to_bitstring(line: &str) -> Vec<u32> {
+    let mut ret = Vec::new();
+    for character in line.chars() {
+        let mut bit = 1u32 << 3;
+        let nibble = nibble(character);
+
+        while bit != 0 {
+            ret.push(if bit & nibble != 0 { 1 } else { 0 });
+
+            bit >>= 1;
+        }
+    }
+    ret
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+        println!("{line}");
+        println!("{:?}", line_to_bitstring(&line));
+    }
 }
