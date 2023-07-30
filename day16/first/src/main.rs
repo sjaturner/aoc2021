f4b68d2723dda5ee7ac55f91ccd55181e8b5490c
sjaturner <sjaturner@googlemail.com>
2023-07-30 18:39:31 +0100

    that seems a little better

--- day16/first/src/main.rs
+++ day16/first/src/main.rs
@@ -24,65 +24,63 @@ fn line_to_bitstring(line: &str) -> Vec<u32> {
     ret
 }
 
-fn tobin(bits: &[u32]) -> u32 {
+struct Chunk<'a> {
+    bits: &'a [u32],
+    offs: usize,
+    rems: i32,
+}
+
+fn tobin(chunk: &mut Chunk, len: i32) -> u32 {
     let mut ret: u32 = 0;
-    let mut count = bits.len();
-    let mut index = 0;
+    let mut len = len;
 
     loop {
-        ret |= bits[index];
-        count -= 1;
-        if count <= 0 {
+        ret |= chunk.bits[chunk.offs];
+        chunk.offs += 1;
+        chunk.rems -= 1;
+        len -= 1;
+
+        if len <= 0 {
             break;
         }
         ret <<= 1;
-        index += 1;
     }
     return ret;
 }
 
-fn literal(bits: &[u32]) -> u32 {
-    let mut offs = 0;
+fn literal(chunk: &mut Chunk) -> u32 {
     let mut ret = 0;
     loop {
-        let cont = bits[offs] == 1;
-        offs += 1;
-        ret |= tobin(&bits[offs..offs + 4]);
+        let cont = tobin(chunk, 1) == 1;
+        ret |= tobin(chunk, 4);
         if !cont {
             break;
         }
         ret <<= 4;
-        offs += 4;
     }
     ret
 }
 
-fn process(bits: &Vec<u32>, pos: usize, depth: u32) {
-    let mut pos = pos;
-    let ver = tobin(&bits[pos..pos + 3]);
-    pos += 3;
-    let typ = tobin(&bits[pos..pos + 3]);
-    pos += 3;
+fn process(chunk: &mut Chunk, _depth: u32) {
+    let ver = tobin(chunk, 3);
+    let typ = tobin(chunk, 3);
     println!("{}", ver);
     println!("{}", typ);
     match typ {
         4 => {
             // Literal
-            println!("literal: {}", literal(&bits[pos..]));
+            println!("literal: {}", literal(chunk));
         }
         _ => {
             // Operator
-            let ltid = bits[pos];
-            pos += 1;
+            let ltid = tobin(chunk, 1);
             match ltid {
                 0 => {
-                    let total_length_of_subpackets = tobin(&bits[pos..pos + 15]);
-                    pos += 15;
+                    let total_length_of_subpackets = tobin(chunk, 15);
                     println!("{}", total_length_of_subpackets);
                 }
                 1 => {
-                    let total_number_of_subpackets = tobin(&bits[pos..pos + 11]);
-                    pos += 11;
+                    let total_number_of_subpackets = tobin(chunk, 11);
                     println!("{}", total_number_of_subpackets);
                 }
                 _ => todo!(),
@@ -98,6 +96,14 @@ fn main() {
         let bitstring = line_to_bitstring(&line);
         println!("{line}");
         println!("{:?}", line_to_bitstring(&line));
-        process(&bitstring, 0, 0);
+        //      process(&bitstring, 0, 0);
+        process(
+            &mut Chunk {
+                bits: &bitstring,
+                offs: 0,
+                rems: bitstring.len() as i32,
+            },
+            0,
+        );
     }
 }
