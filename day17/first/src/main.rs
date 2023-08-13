d9cb4f72b425c49f5956368fa49664e934abc879
sjaturner <sjaturner@googlemail.com>
2023-08-13 13:13:29 +0100

    does the trick

--- day17/first/src/main.rs
+++ day17/first/src/main.rs
@@ -38,10 +38,15 @@ fn step(vel: &mut Velocity, pos: &mut Position) {
 }
 
 fn main() {
-    let target = Target {
+    let _target = Target {
         x: Range { lo: 20, hi: 30 },
         y: Range { lo: -10, hi: -5 },
     };
+    let target = Target {
+        x: Range { lo: 227, hi: 318 },
+        y: Range { lo: -92, hi: -53 },
+    };
+
     let mut candidates_velx = Vec::new();
     for velx in 1..=target.x.hi + 1 {
         let mut vel = Velocity { x: velx, y: 0 };
@@ -61,22 +66,26 @@ fn main() {
         }
     }
     //  let mut candidates_vel = Vec::new();
+    let mut maxy = 0;
     for velx in candidates_velx {
         let mut vely = 1;
+        let mut vely_maxy = 0;
 
         'vely_loop: loop {
+            if -vely < target.y.lo {
+                break 'vely_loop;
+            }
             let mut vel = Velocity { x: velx, y: vely };
             let mut pos = Position { x: 0, y: 0 };
-
+            println!("{velx} {vely} {:?}", pos);
             loop {
-                println!("hit {velx} {vely} {:?}", pos);
-
                 if pos.x >= target.x.lo
                     && pos.x <= target.x.hi
                     && pos.y >= target.y.lo
                     && pos.y <= target.y.hi
                 {
-                    println!("hit {velx} {vely}");
+                    println!("hit {velx} {vely} {:?}", pos);
+                    maxy = if vely_maxy > maxy { vely_maxy } else { maxy };
                     break;
                 }
                 if pos.y > target.y.hi && pos.x > target.x.hi {
@@ -88,9 +97,12 @@ fn main() {
                 if pos.x > target.x.hi {
                     break;
                 }
+                vely_maxy = if pos.y > vely_maxy { pos.y } else { vely_maxy };
+
                 step(&mut vel, &mut pos);
             }
             vely += 1;
         }
     }
+    println!("{maxy}");
 }
