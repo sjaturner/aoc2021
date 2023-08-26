cd521205d43e63fb0e35e5ec4266d50cd7926b02
Simon Turner <sjaturner@googlemail.com>
2023-08-26 16:28:47 +0100

    https://rusty-ferris.pages.dev/blog/binary-tree-sum-of-values/ first approximation

--- day18/first/src/main.rs
+++ day18/first/src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, BufRead};
+use std::{cell::RefCell, rc::Rc};
+
+type TreeNodeRef = Rc<RefCell<TreeNode>>;
+
+#[derive(Debug, Clone)]
+pub struct TreeNode {
+    val: i32,
+    left: Option<TreeNodeRef>,
+    right: Option<TreeNodeRef>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+
+        println!("{line}");
+    }
 }
