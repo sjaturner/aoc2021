145c3b2e8ef304099aee0ad3b351dd358cbdc0e3
Simon Turner <sjaturner@googlemail.com>
2023-08-26 17:48:26 +0100

    all of the right notes - but not necessarily in the right order

--- day18/first/src/main.rs
+++ day18/first/src/main.rs
@@ -15,7 +15,41 @@ pub struct TreeNode {
     r: Elem,
 }
 
-fn build(input: &str, pos: &mut usize) -> TreeNode {
+fn char_at(input: &[u8], pos: &mut usize) -> char {
+    input[*pos] as char
+}
+
+fn eat_space(input: &[u8], pos: &mut usize) {
+    while char_at(input, pos).is_whitespace() {
+        *pos += 1;
+    }
+}
+fn build_elem(input: &[u8], pos: &mut usize) -> Elem {
+    eat_space(input, pos);
+    match char_at(input, pos) {
+        '0'..='9' => Elem::Val(0),
+        '[' => Elem::Val(0),
+        _ => {
+            todo!();
+        }
+    }
+}
+
+fn build_tree_node(input: &[u8], pos: &mut usize) -> TreeNode {
+    eat_space(input, pos);
+    match char_at(input, pos) {
+        '[' => {
+            *pos += 1;
+            build_elem(input, pos);
+            eat_space(input, pos);
+            assert!(char_at(input, pos) == ',');
+            build_elem(input, pos);
+            eat_space(input, pos);
+            assert!(char_at(input, pos) == ']');
+        }
+        _ => {}
+    }
+
     return TreeNode {
         l: Elem::Val(0),
         r: Elem::Val(0),
@@ -28,5 +62,7 @@ fn main() {
         let line = line.expect("Could not read line from standard in");
 
         println!("{line}");
+
+        let tree = build_tree_node(line.as_bytes(), &mut 0);
     }
 }
