42c23f9ef53d5af3a9bd3def6f45147cc2eeb240
Simon Turner <sjaturner@googlemail.com>
2023-08-27 15:30:13 +0100

    can only think that this is the wrong way to go about it, looks yuck

--- day18/first/src/main.rs
+++ day18/first/src/main.rs
@@ -1,10 +1,105 @@
 use std::io::{self, BufRead};
 
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Token {
+    Open,
+    Close,
+    Val(i32),
+}
+
+fn reduce(ip: Vec<Token>) -> Option<Vec<Token>> {
+    let mut nesting = 0;
+    let mut num_first: Option<usize> = None;
+    let mut triggered: Option<usize> = None;
+    let mut num_last: Option<usize> = None;
+
+    for index in 0..ip.len() {
+        if triggered == None {
+            nesting += if ip[index] == Token::Open {
+                1
+            } else if ip[index] == Token::Close {
+                -1
+            } else {
+                0
+            };
+
+            if let Token::Val(_) = ip[index] {
+                num_first = Some(index);
+            }
+
+            if nesting >= 4 {
+                if index < ip.len() - 4 {
+                    match ip[index..index + 4] {
+                        [Token::Open, Token::Val(_), Token::Val(_), Token::Close] => {
+                            triggered = Some(index);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        } else if num_last == None {
+            let trigger_index = triggered.unwrap();
+
+            if index > trigger_index + 4 {
+                if let Token::Val(_) = ip[index] {
+                    num_last = Some(index);
+                }
+            }
+        }
+    }
+
+    if triggered == None {
+        None
+    } else {
+        let mut result = Vec::new();
+        let trigger_index = triggered.unwrap();
+
+        for index in 0..ip.len() {
+            if index == trigger_index {
+                result.push(Token::Val(0));
+            } else if index > trigger_index && index < trigger_index + 4 {
+            } else if Some(index) == num_first {
+                if let Token::Val(a) = ip[trigger_index + 1] {
+                    if let Token::Val(b) = ip[index] {
+                        result.push(Token::Val(a + b));
+                    }
+                }
+            } else if Some(index) == num_last {
+                if let Token::Val(a) = ip[trigger_index + 2] {
+                    if let Token::Val(b) = ip[index] {
+                        result.push(Token::Val(a + b));
+                    }
+                }
+            } else {
+                result.push(ip[index]);
+            }
+        }
+
+        Some(result)
+    }
+}
+
 fn main() {
     let stdin = io::stdin();
+    let mut tokens = Vec::new();
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from standard in");
 
-        println!("{line}");
+        for c in line.chars() {
+            match c {
+                '[' => {
+                    tokens.push(Token::Open);
+                }
+                ']' => {
+                    tokens.push(Token::Close);
+                }
+                '0'..='9' => tokens.push(Token::Val(c as i32 - '0' as i32)),
+                ',' => {}
+                _ => {
+                    todo!();
+                }
+            }
+        }
+        println!("{:?}", tokens);
     }
 }
