847e376b2064fd835d74dddea90a413abd67248f
Simon Turner <sjaturner@googlemail.com>
2023-11-25 18:06:53 +0000

    only slice up affected blocks, much quicker but the point overlap detect is a bit ugly and repetetive

--- day22/second/src/main.rs
+++ day22/second/src/main.rs
@@ -125,7 +125,76 @@ mod tests {
     }
 }
 
+fn point_in_block(x: i32, y: i32, z: i32, block: Block) -> bool {
+    return true
+        && x >= block.dim_range[0].0
+        && x <= block.dim_range[0].1
+        && y >= block.dim_range[1].0
+        && y <= block.dim_range[1].1
+        && z >= block.dim_range[2].0
+        && z <= block.dim_range[2].1;
+}
+
+fn is_overlapped(is_this: Block, overlapped_with: Block) -> bool {
+    return false
+        || point_in_block(
+            is_this.dim_range[0].0,
+            is_this.dim_range[1].0,
+            is_this.dim_range[2].0,
+            overlapped_with,
+        )
+        || point_in_block(
+            is_this.dim_range[0].0,
+            is_this.dim_range[1].0,
+            is_this.dim_range[2].1,
+            overlapped_with,
+        )
+        || point_in_block(
+            is_this.dim_range[0].0,
+            is_this.dim_range[1].1,
+            is_this.dim_range[2].0,
+            overlapped_with,
+        )
+        || point_in_block(
+            is_this.dim_range[0].0,
+            is_this.dim_range[1].1,
+            is_this.dim_range[2].1,
+            overlapped_with,
+        )
+        || point_in_block(
+            is_this.dim_range[0].1,
+            is_this.dim_range[1].0,
+            is_this.dim_range[2].0,
+            overlapped_with,
+        )
+        || point_in_block(
+            is_this.dim_range[0].1,
+            is_this.dim_range[1].0,
+            is_this.dim_range[2].1,
+            overlapped_with,
+        )
+        || point_in_block(
+            is_this.dim_range[0].1,
+            is_this.dim_range[1].1,
+            is_this.dim_range[2].0,
+            overlapped_with,
+        )
+        || point_in_block(
+            is_this.dim_range[0].1,
+            is_this.dim_range[1].1,
+            is_this.dim_range[2].1,
+            overlapped_with,
+        );
+}
+
 fn block_slice(victim: Block, block: Block) -> Vec<Block> {
+    if !is_overlapped(victim, block) {
+        let mut ret = Vec::new();
+
+        ret.push(victim);
+        return ret;
+    }
+
     let a = dim_slice(victim, 0, block.dim_range[0]);
 
     let mut b = Vec::new();
@@ -184,8 +253,6 @@ fn main() {
         let line = line.expect("Could not read line from standard in");
         let line = line.as_str();
 
-        println!("{}", index);
-
         let r = Regex::new(r"(off|on) x=([-+0-9]*)\.\.([-+0-9]*),y=([-+0-9]*)\.\.([-+0-9]*),z=([-+0-9]*)\.\.([-+0-9]*)").unwrap();
         let caps = r.captures(line).unwrap();
         let on_off = &caps[1];
@@ -215,6 +282,6 @@ fn main() {
                 state.push(block);
             }
         }
+        println!("{} {}", index, state.len());
     }
-    println!("{}", state.len());
 }
