a62223d0ab0d648c43e23e183b6ac42ff0285500
Simon Turner <sjaturner@googlemail.com>
2023-12-02 11:53:58 +0000

    move some stuff into the block object

--- day22/second/src/main.rs
+++ day22/second/src/main.rs
@@ -6,24 +6,6 @@ struct Block {
     dim_range: [(i32, i32); 3],
 }
 
-fn point_in_block(x: i32, y: i32, z: i32, block: Block) -> bool {
-    x >= block.dim_range[0].0
-        && x <= block.dim_range[0].1
-        && y >= block.dim_range[1].0
-        && y <= block.dim_range[1].1
-        && z >= block.dim_range[2].0
-        && z <= block.dim_range[2].1
-}
-
-fn b_fully_contains_a(a: Block, b: Block) -> bool {
-    a.dim_range[0].0 >= b.dim_range[0].0
-        && a.dim_range[0].1 <= b.dim_range[0].1
-        && a.dim_range[1].0 >= b.dim_range[1].0
-        && a.dim_range[1].1 <= b.dim_range[1].1
-        && a.dim_range[2].0 >= b.dim_range[2].0
-        && a.dim_range[2].1 <= b.dim_range[2].1
-}
-
 impl Block {
     fn overlaps(&self, other: Block) -> bool {
         let xl1 = self.dim_range[0].0;
@@ -54,78 +36,76 @@ impl Block {
 
         xl * yl * zl
     }
-}
 
-fn push_check(vec: &mut Vec<Block>, block: Block) {
-    if block.dim_range[0].0 > block.dim_range[0].1
-        || block.dim_range[1].0 > block.dim_range[1].1
-        || block.dim_range[2].0 > block.dim_range[2].1
-    {
-        assert!(false);
+    fn fully_contains(&self, block: Block) -> bool {
+        block.dim_range[0].0 >= self.dim_range[0].0
+            && block.dim_range[0].1 <= self.dim_range[0].1
+            && block.dim_range[1].0 >= self.dim_range[1].0
+            && block.dim_range[1].1 <= self.dim_range[1].1
+            && block.dim_range[2].0 >= self.dim_range[2].0
+            && block.dim_range[2].1 <= self.dim_range[2].1
     }
 
-    vec.push(block);
-}
+    fn dim_slice(&self, dim: usize, range: (i32, i32)) -> Vec<Block> {
+        let mut ret = Vec::new();
 
-fn dim_slice(victim: Block, dim: usize, range: (i32, i32)) -> Vec<Block> {
-    let mut ret = Vec::new();
+        let victim_dim_range = self.dim_range[dim];
 
-    let victim_dim_range = victim.dim_range[dim];
+        if victim_dim_range.1 < range.0
+            || victim_dim_range.0 > range.1
+            || victim_dim_range.0 > range.0 && victim_dim_range.1 < range.1
+        {
+            ret.push(*self);
+        } else if victim_dim_range.0 < range.0 && victim_dim_range.1 > range.1 {
+            let mut l = *self;
 
-    if victim_dim_range.1 < range.0
-        || victim_dim_range.0 > range.1
-        || victim_dim_range.0 > range.0 && victim_dim_range.1 < range.1
-    {
-        push_check(&mut ret, victim);
-    } else if victim_dim_range.0 < range.0 && victim_dim_range.1 > range.1 {
-        let mut l = victim;
+            l.dim_range[dim].1 = range.0 - 1;
+            ret.push(l);
 
-        l.dim_range[dim].1 = range.0 - 1;
-        push_check(&mut ret, l);
+            let mut m = *self;
+            m.dim_range[dim].0 = range.0;
+            m.dim_range[dim].1 = range.1;
+            ret.push(m);
 
-        let mut m = victim;
-        m.dim_range[dim].0 = range.0;
-        m.dim_range[dim].1 = range.1;
-        push_check(&mut ret, m);
+            let mut r = *self;
+            r.dim_range[dim].0 = range.1 + 1;
+            ret.push(r);
+        } else if victim_dim_range.1 > range.1 {
+            let mut l = *self;
+            l.dim_range[dim].1 = range.1;
+            ret.push(l);
 
-        let mut r = victim;
-        r.dim_range[dim].0 = range.1 + 1;
-        push_check(&mut ret, r);
-    } else if victim_dim_range.1 > range.1 {
-        let mut l = victim;
-        l.dim_range[dim].1 = range.1;
-        push_check(&mut ret, l);
+            let mut r = *self;
+            r.dim_range[dim].0 = range.1 + 1;
+            ret.push(r);
+        } else if victim_dim_range.0 < range.0 {
+            let mut l = *self;
+            l.dim_range[dim].1 = range.0 - 1;
+            ret.push(l);
 
-        let mut r = victim;
-        r.dim_range[dim].0 = range.1 + 1;
-        push_check(&mut ret, r);
-    } else if victim_dim_range.0 < range.0 {
-        let mut l = victim;
-        l.dim_range[dim].1 = range.0 - 1;
-        push_check(&mut ret, l);
+            let mut r = *self;
+            r.dim_range[dim].0 = range.0;
+            ret.push(r);
+        } else {
+            ret.push(*self);
+        }
 
-        let mut r = victim;
-        r.dim_range[dim].0 = range.0;
-        push_check(&mut ret, r);
-    } else {
-        push_check(&mut ret, victim);
+        ret
     }
-
-    ret
 }
 
 fn block_slice(victim: Block, block: Block) -> Vec<Block> {
     if victim.overlaps(block) {
-        let a = dim_slice(victim, 0, block.dim_range[0]);
+        let a = victim.dim_slice(0, block.dim_range[0]);
 
         let mut b = Vec::new();
         for scan in a {
-            b.extend(dim_slice(scan, 1, block.dim_range[1]));
+            b.extend(scan.dim_slice(1, block.dim_range[1]));
         }
 
         let mut c = Vec::new();
         for scan in b {
-            c.extend(dim_slice(scan, 2, block.dim_range[2]));
+            c.extend(scan.dim_slice(2, block.dim_range[2]));
         }
 
         c
@@ -141,21 +121,10 @@ fn volume(state: &Vec<Block>) -> u64 {
 fn slice(state: &Vec<Block>, block: Block) -> Vec<Block> {
     let mut ret = Vec::new();
 
-    let before = volume(state);
-
     for scan in state {
         let sliced = block_slice(*scan, block);
 
         ret.extend(sliced);
-    }
-
-    let after = volume(&ret);
-
-    if before != after {
-        println!("{}", before);
-        println!("{}", after);
-        println!();
-        assert!(false);
     }
 
     ret
@@ -184,19 +153,19 @@ fn main() {
         };
 
         if state.is_empty() && on_off == "on" {
-            push_check(&mut state, block);
+            state.push(block);
         } else {
             state = slice(&state, block);
             let mut filtered = Vec::new();
             for elem in state {
-                if b_fully_contains_a(elem, block) {
+                if block.fully_contains(elem) {
                 } else {
-                    push_check(&mut filtered, elem);
+                    filtered.push(elem);
                 }
             }
             state = filtered;
             if on_off == "on" {
-                push_check(&mut state, block);
+                state.push(block);
             }
         }
     }
