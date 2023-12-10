3b4abca886e171fb254d0d3b46a89e8f3e6aedd8
Simon Turner <sjaturner@googlemail.com>
2023-12-10 17:44:01 +0000

    a bit yuck

--- day23/first/src/main.rs
+++ day23/first/src/main.rs
@@ -7,7 +7,7 @@ const D: usize = 3;
 const COLOURS: usize = 4;
 
 struct State {
-    pos: [[(u32, u32); COLOURS as usize]; 2],
+    pos: [[(u32, u32); COLOURS]; 2],
 }
 
 impl State {
@@ -31,7 +31,43 @@ fn main() {
             if outer == inner {
                 continue;
             }
-            connections.entry(outer).or_insert(Vec::new()).push(inner);
+            connections.entry(outer).or_default().push(inner);
+        }
+    }
+
+    let mut connections: HashMap<(i32, i32), [Vec<((i32, i32), i32)>; COLOURS]> = HashMap::new();
+
+    for col in 0..=10 {
+        if positions.contains(&(1, col)) {
+            for colour in 0..COLOURS {
+                let mut l: Option<(i32, i32)> = Some((0, col));
+                let mut r: Option<(i32, i32)> = Some((0, col));
+                let mut d: Option<(i32, i32)> = None;
+
+                loop {
+                    if let Some(pos) = l {
+                        let mut l_col = pos.1 - 1;
+
+                        if d.is_none() && l_col == 2 + colour as i32 * 2 {
+                            d = Some((1, l_col));
+                        } else {
+                            d = match d {
+                                Some((1, c)) => Some((2, c)),
+                                Some((2, _)) => None,
+                                _ => None,
+                            }
+                        }
+
+                        l_col = match l_col {
+                            2 | 4 | 6 | 8 => l_col - 1,
+                            _ => l_col,
+                        };
+                        if l_col < 0 {
+                            l = None;
+                        }
+                    }
+                }
+            }
         }
     }
 }
