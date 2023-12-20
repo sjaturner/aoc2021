94d7df56a62f9ddf35a37d0eecc70af52da499b5
Simon Turner <sjaturner@googlemail.com>
2023-12-20 16:14:27 +0000

    well that is the easy bit

--- day23/first/src/main.rs
+++ day23/first/src/main.rs
@@ -1,3 +1,29 @@
+use std::io::{self, BufRead};
+
+struct Board {
+    tile: Vec<Vec<char>>,
+}
+
+impl Board {
+    fn show(self) {
+        for row in self.tile {
+            for elem in row {
+                print!("{elem}");
+            }
+            println!();
+        }
+    }
+}
+
 fn main() {
-    println!("Starting to hate this ...");
+    let mut board = Board {
+        tile: Vec::new()
+    };
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let line = line.expect("Could not read line from standard in");
+        println!("{}", line);
+        board.tile.push(line.chars().collect());
+    }
+    board.show();
 }
